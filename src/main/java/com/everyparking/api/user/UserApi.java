d6e305486782c8f2034c524a0a708656ec729533
taewoo <gimgau0218@naver.com>
2022-09-24 22:21:03 +0900

    feat: UserController

--- src/main/java/com/everyparking/api/user/UserApi.java
+++ src/main/java/com/everyparking/api/user/UserApi.java
@@ -6,24 +6,29 @@ package com.everyparking.api.user;
 
 
 import com.everyparking.dto.registry.RegistryRequestDto;
+import com.everyparking.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @Slf4j
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class UserApi {
 
+    private final UserService userService;
+
     @PostMapping("")
     public ResponseEntity<?> registyUser(@Valid @RequestBody RegistryRequestDto registryDto) {
+        log.info("회원가입 요청: " + registryDto.getEmail());
 
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.registryUser(registryDto));
     }
 }
--- src/main/java/com/everyparking/config/WebSecurityConfig.java
+++ src/main/java/com/everyparking/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,7 +27,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.authorizeRequests().antMatchers("/api/*").permitAll();
+        http.authorizeRequests().antMatchers("/*").permitAll();
+        http.authorizeRequests().antMatchers("/swagger-resources/**").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",  "/configuration/ui",
+                "/swagger-resources", "/configuration/security",
+                "/swagger-ui.html", "/webjars/**","/swagger/**");
     }
 
     @Bean
