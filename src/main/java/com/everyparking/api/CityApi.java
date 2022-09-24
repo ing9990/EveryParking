cac37dbb2fc25438f67d488055d5c4af653e1e2d
taewoo <gimgau0218@naver.com>
2022-09-24 22:22:42 +0900

    feat: City 목록 Api 추가

---
+++ src/main/java/com/everyparking/api/CityApi.java
@@ -0,0 +1,39 @@
+package com.everyparking.api;
+
+/**
+ * @author Taewoo
+ */
+
+
+import com.everyparking.data.user.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class CityApi {
+
+    @GetMapping
+    public ResponseEntity getCities(@RequestParam(required = false) Integer listSize) {
+        log.info("City 요청");
+        if (listSize == null) return ResponseEntity.ok().body(User.City.values());
+        return ResponseEntity.ok().body(Arrays.stream(User.City.values()).limit(listSize));
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
