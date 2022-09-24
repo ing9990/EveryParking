6b287177283429e4e8928eeb4edac8e16662c76d
taewoo <gimgau0218@naver.com>
2022-09-24 22:21:14 +0900

    feat: 유저 도메인

--- src/main/java/com/everyparking/data/user/domain/User.java
+++ src/main/java/com/everyparking/data/user/domain/User.java
@@ -57,4 +57,17 @@ public class User {
     public static enum City {
         서울특별시, 부산광역시, 대구광역시, 인천광역시, 광주광역시, 대전광역시, 울산광역시, 세종특별자치시, 경기도, 강원도, 충청북도, 충청남도, 전라북도, 전라남도, 경상북도, 경상남도, 제주특별자치도, 미정
     }
+
+    public static User makeUser(String email, String password, String nickname, String tel, String introduce, City city) {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .tel(tel)
+                .introduce(introduce)
+                .city(city)
+                .created(LocalDateTime.now())
+                .updated(LocalDateTime.now())
+                .build();
+    }
 }
