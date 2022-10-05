aca316e1179868c0a75d9e98865257a767bc0d90
taewoo <gimgau0218@naver.com>
2022-10-05 11:00:40 +0900

    feat: LoginRequestDto.java valid message 추가

--- src/main/java/com/everyparking/api/dto/login/LoginRequestDto.java
+++ src/main/java/com/everyparking/api/dto/login/LoginRequestDto.java
@@ -14,6 +14,8 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -21,10 +23,11 @@ import javax.validation.constraints.NotBlank;
 @Builder
 public class LoginRequestDto {
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일이 4빈칸입니다.")
+    @Email(message = "이메일 포맷이 아닙니다.")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호가 빈칸입니다.")
+    @Size(min = 7, max = 14, message = "7자리 이상 14자리 비밀번호가 아닙니다.")
     private String password;
 }
