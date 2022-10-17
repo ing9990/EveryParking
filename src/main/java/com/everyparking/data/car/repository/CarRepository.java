5e48509e3b8d60e94df37caffc62226a73257df6
taewoo <gimgau0218@naver.com>
2022-10-17 13:30:52 +0900

    feat: 자동차 번호판으로 자동차 검색

--- src/main/java/com/everyparking/data/car/repository/CarRepository.java
+++ src/main/java/com/everyparking/data/car/repository/CarRepository.java
@@ -3,6 +3,7 @@ package com.everyparking.data.car.repository;
 import com.everyparking.data.car.domain.Car;
 import com.everyparking.data.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,6 +12,12 @@ import java.util.List;
  */
 
 public interface CarRepository extends JpaRepository<Car, Long> {
- boolean existsByCarNumber(String carNumber);
- List<Car> findCarsByUser(User user);
+
+    boolean existsByCarNumber(String carNumber);
+
+    @Query("select c from Car c where c.user = ?1")
+    List<Car> findCarsByUser(User user);
+
+    @Query("select c from Car c where c.carNumber = ?1")
+    Car findCarByCarNumber(String carNumber);
 }
