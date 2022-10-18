7245073754536ea47ec2d2aaa2f27448cf379bdd
taewoo <gimgau0218@naver.com>
2022-10-18 20:18:25 +0900

    feat: Exception Handling 추가

--- src/main/java/com/everyparking/exception/global/GlobalControllerAdvice.java
+++ src/main/java/com/everyparking/exception/global/GlobalControllerAdvice.java
@@ -1,9 +1,6 @@
 package com.everyparking.exception.global;
 
-import com.everyparking.exception.EmailNotFoundException;
-import com.everyparking.exception.InvalidAuthenticationException;
-import com.everyparking.exception.PasswordNotMatchException;
-import com.everyparking.exception.RentTimeInvalidException;
+import com.everyparking.exception.*;
 import com.everyparking.exception.dto.Error;
 import com.everyparking.exception.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +38,17 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(RentTimeInvalidException.class)
     public ResponseEntity<?> invalidTime(HttpServletRequest request, RentTimeInvalidException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(Error
+                        .builder()
+                        .path(request.getRequestURI())
+                        .message(e.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(BeShortOfPointException.class)
+    public ResponseEntity<?> invalidTime(HttpServletRequest request, BeShortOfPointException e) {
         return ResponseEntity
                 .badRequest()
                 .body(Error
