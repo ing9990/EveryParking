4da28499fe4c46cebb176c3fc6252d63002f0227
taewoo <gimgau0218@naver.com>
2022-10-18 20:20:52 +0900

    feat: 수정된 테이블에 맞도록 JPQL 변경

--- src/main/java/com/everyparking/data/rent/repository/RentRepository.java
+++ src/main/java/com/everyparking/data/rent/repository/RentRepository.java
@@ -3,7 +3,9 @@ package com.everyparking.data.rent.repository;
 
 import com.everyparking.data.rent.domain.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,11 +18,16 @@ public interface RentRepository extends JpaRepository<Rent, Long> {
     List<Rent> getRentsByStartGreaterThanEqualOrderByStartAsc(LocalDateTime start);
 
 
-    @Query("select r " + "from Rent r " + "where r.end >= ?1 and r.isBorrowed = false and r.place.user.id <> ?2 " + "order by r.start")
+    @Query("select r " + "from Rent r " + "where r.end >= ?1 and r.rentStatus <> 'inUse' and r.place.user.id <> ?2 " + "order by r.start")
     List<Rent> getRecommandLists(LocalDateTime end, Long userId);
 
-    @Query("select r " + "from Rent r " + "where r.place.user.id <> ?1 and r.isBorrowed = false")
+    @Query("select r " + "from Rent r " + "where r.place.user.id <> ?1 and r.rentStatus <> 'inUse'")
     List<Rent> findRentsByNotUserId(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Rent r where r.place.id = ?1")
+    void deleteRentByPlaceId(Long placeId);
 }
 
 
