1f24ff3e237c23aeb2208a54cbd01f04f4dbb725
taewoo <gimgau0218@naver.com>
2022-10-18 20:22:46 +0900

    feat: 주차장 대여 로직 추가

--- src/main/java/com/everyparking/data/rent/service/RentService.java
+++ src/main/java/com/everyparking/data/rent/service/RentService.java
@@ -16,10 +16,12 @@ import com.everyparking.exception.PlaceNotFoundException;
 import com.everyparking.exception.RentTimeInvalidException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -44,8 +46,6 @@ public class RentService {
     @Transactional
     public DefaultResponseDtoEntity addRent(String authorization, AddRentDto addRentDto) {
 
-        log.info(addRentDto.getEndTime().toString());
-
         var place = placeService.findById(addRentDto.getPlaceId()).orElseThrow(PlaceNotFoundException::new);
 
         var user = jwtTokenUtils.getUserByToken(authorization);
@@ -53,9 +53,9 @@ public class RentService {
         if (LocalDateTime.now().isAfter(addRentDto.getStartTime()) || LocalDateTime.now().isAfter(addRentDto.getEndTime()) || addRentDto.getStartTime().isAfter(addRentDto.getEndTime()))
             throw new RentTimeInvalidException("대여 시간이 정확하지 않습니다.");
 
-        var rent = Rent.dtoToEntity(place, addRentDto.getCost(), addRentDto.getMessage(), false, addRentDto.getStartTime().minusHours(3).plusSeconds(49), addRentDto.getEndTime().minusHours(3).plusSeconds(49));
+        var rent = Rent.dtoToEntity(place, addRentDto.getCost(), addRentDto.getMessage(), Rent.RentStatus.waiting, addRentDto.getStartTime().minusHours(3).plusSeconds(49), addRentDto.getEndTime().minusHours(3).plusSeconds(49));
 
-        placeService.updateBorrow(place);
+        placeService.updateBorrow(place, Place.PlaceStatus.pending);
 
         return DefaultResponseDtoEntity.of(HttpStatus.CREATED, "렌트 등록 성공", rentRepository.save(rent));
     }
@@ -79,5 +79,46 @@ public class RentService {
     @Transactional(readOnly = true)
     public List<Rent> findRentsByNotUserId(Long id) {
         return rentRepository.findRentsByNotUserId(id);
+    }
+
+    @Transactional
+    public DefaultResponseDtoEntity cancelRent(String authorization, Long placeId) {
+        var user = jwtTokenUtils.getUserByToken(authorization);
+        var place = placeService.updateStatus(placeId);
+
+        rentRepository.deleteRentByPlaceId(placeId);
+
+        return DefaultResponseDtoEntity.ok("취소되었습니다. [" + place.getName() + "]");
+    }
+
+    public Rent findRentById(Long rentId) {
+        return rentRepository.findById(rentId).orElseThrow(PlaceNotFoundException::new);
+    }
+
+    public boolean comparePoint(long cost, long point) {
+        return cost > point;
+    }
+
+    @Transactional
+    @Modifying
+    public void updateStatus(Rent rent) {
+        rent.setRentStatus(Rent.RentStatus.inUse);
+        rentRepository.save(rent);
+    }
+
+    public Duration compareEndTime(LocalDateTime endTime, Rent rent) {
+        return Duration.between(endTime, rent.getEnd());
+    }
+
+    public Duration compareEndTime(LocalDateTime start, LocalDateTime endTime) {
+        return Duration.between(endTime, start);
+    }
+
+
+    @Transactional
+    @Modifying
+    public void updateStartTime(Rent rent, LocalDateTime endAt) {
+        rent.setStart(endAt);
+        rentRepository.save(rent);
     }
 }
