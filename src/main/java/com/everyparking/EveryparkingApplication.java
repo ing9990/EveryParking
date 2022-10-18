5f59fecf26cf479d0aaa4441263b5cb31b505d18
taewoo <gimgau0218@naver.com>
2022-10-18 20:18:07 +0900

    feat: 스케쥴러 활성화 refactor: unused 정리

--- src/main/java/com/everyparking/EveryparkingApplication.java
+++ src/main/java/com/everyparking/EveryparkingApplication.java
@@ -2,11 +2,10 @@ package com.everyparking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EveryparkingApplication {
 
     public static void main(String[] args) {
