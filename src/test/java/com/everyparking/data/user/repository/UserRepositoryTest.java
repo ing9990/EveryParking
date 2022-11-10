df965d532d6b9baf05a30b42fd026df5bdb96300
taewoo <gimgau0218@naver.com>
2022-11-10 15:18:43 +0900

    test: User 데이터베이스 테스트 코드 추가

---
+++ src/test/java/com/everyparking/data/user/repository/UserRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.everyparking.data.user.repository;
+
+import com.everyparking.api.dto.LoginRequestDto;
+import com.everyparking.data.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Taewoo
+ */
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("1.[Not Null] UserRepository")
+    @Test
+    void NN_유저_전체_테스트() {
+        assertNotNull(userRepository);
+    }
+
+    @DisplayName("2.[Not Null] User")
+    @Test
+    void NN_유저_테스트() {
+        var user = userRepository.findAll().get(0);
+        assertNotNull(user);
+    }
+
+    @DisplayName("3.[Not Null] userInfo")
+    @Test
+    void NN_유저_정보_테스트() {
+        var email = userRepository.findAll().get(0).getEmail();
+        assertNotNull(email);
+    }
+}
